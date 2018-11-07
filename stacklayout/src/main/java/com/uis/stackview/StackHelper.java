cedf874fcfdd25fafb0b4400fa07eb030a86e78d
uis <huaibing.liu@bl.com>
2018-11-07 14:54:00 +0800

    更改属性

--- stacklayout/src/main/java/com/uis/stackview/StackHelper.java
+++ stacklayout/src/main/java/com/uis/stackview/StackHelper.java
@@ -70,8 +70,12 @@ final class StackHelper implements ValueAnimator.AnimatorUpdateListener{
                 float stackSpaces = 0;
                 everyHeight = height - layout.getPaddingTop() - layout.getPaddingBottom();
                 originX.add(layout.stackEdge);
+                int padX = layout.stackPadX;
+                if(padX*(size-1) > layout.stackSpace){
+                    padX = 0;
+                }
                 for (int i = 1; i < size; i++) {
-                    int stackSpace = (int)(layout.stackSpace /(1+layout.stackZoomX*(size-1-i)));
+                    int stackSpace = (int)((layout.stackSpace-padX*(size-1-i))*Math.pow(layout.stackZoomX,(size-1-i)));
                     stackSpaces += stackSpace;
                     originX.add(originX.get(i - 1) + stackSpace);
                 }
@@ -98,6 +102,7 @@ final class StackHelper implements ValueAnimator.AnimatorUpdateListener{
             needRelayout = false;
             int childSize = layout.getChildCount();
             int stackSize = layout.stackSize;
+            int realSize = layout.getRealStackSize();
             for (int i = 0; i < childSize; i++) {
                 int top, bottom, left, right, pivot;
                 View view = layout.getChildAt(i);
@@ -117,7 +122,7 @@ final class StackHelper implements ValueAnimator.AnimatorUpdateListener{
                     view.setPivotX(pivot);
                     view.setPivotY(everyHeight / 2);
                     view.layout(left, top, right, bottom);
-                    view.setScaleY(getChildScale(i, layout.stackZoomY));
+                    view.setScaleY((float) Math.pow(layout.stackZoomY,realSize-1-i));
                     if(view.getTranslationX() != 0f){
                         if(i+1 < stackSize){
                             view.setTranslationX(0);
@@ -191,11 +196,6 @@ final class StackHelper implements ValueAnimator.AnimatorUpdateListener{
             view.setScaleY(1f);
             weakViews.add(new WeakReference<>(view));
         }
-    }
-
-    private float getChildScale(int index,float zoom) {
-        int rate = layout.getRealStackSize() -1 - index;
-        return (float) Math.pow(1.0f - zoom,rate);
     }
 
     /**
--- stacklayout/src/main/java/com/uis/stackview/StackLayout.java
+++ stacklayout/src/main/java/com/uis/stackview/StackLayout.java
@@ -24,10 +24,11 @@ public class StackLayout extends ViewGroup{
     int stackSpace = 10;
     /** 层叠视图边距 */
     int stackEdge = 0;
-    /** 层叠递减倍数 0.0-2.0*/
-    float stackZoomX = 0.1f;
-    /** 层叠缩放比例 0.0-0.2*/
-    float stackZoomY = 0.1f;
+    /** 层叠递减倍数 >0*/
+    float stackZoomX = 1f;
+    int stackPadX = 0;
+    /** 层叠缩放比例 >0*/
+    float stackZoomY = 0.9f;
     /** 层叠显示数量 */
     int stackSize = 3;
     /** 自动轮播 */
@@ -58,6 +59,7 @@ public class StackLayout extends ViewGroup{
         stackSpace =  (int)type.getDimension(R.styleable.StackLayout_stackSpace, stackSpace);
         stackEdge = (int)type.getDimension(R.styleable.StackLayout_stackEdge,stackEdge);
         stackZoomX = type.getFloat(R.styleable.StackLayout_stackZoomX, stackZoomX);
+        stackPadX = (int)type.getDimension(R.styleable.StackLayout_stackPadX,stackPadX);
         stackZoomY = type.getFloat(R.styleable.StackLayout_stackZoomY, stackZoomY);
         stackSize = type.getInteger(R.styleable.StackLayout_stackSize,stackSize);
         stackLooper = type.getBoolean(R.styleable.StackLayout_stackLooper,stackLooper);
