25d511e10271aeb62c5f3f94eecdc742e61adb62
uis <luiing@foxmail.com>
2020-06-04 05:31:04 +0800

    重构scroll

--- demo/src/main/java/com/uis/stackviewlayout/demo/activity/StackAdapter.java
+++ demo/src/main/java/com/uis/stackviewlayout/demo/activity/StackAdapter.java
@@ -44,7 +44,7 @@ public class StackAdapter extends RecyclerView.Adapter<StackAdapter.StackVH> {
 
     @Override
     public int getItemCount() {
-        return 20;
+        return 2;
     }
 
     public static ArrayList<ItemEntity> initDataList(Context context) {
